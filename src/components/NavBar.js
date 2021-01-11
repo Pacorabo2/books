022b8ca99a2ee71af3806c0bd9f59bd39c1e9128
Nicolas Plaza <pacorabo2@hotmail.com>
2021-01-11 21:50:29 +0100

    NavBar implementation

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBAr = () => {
+  return (
+    <header>
+      <div className="d-flex flex-column felx-md-row p-3 border-bottom bg-secondary text-white">
+        <h4 className="mr-md-auto">
+          <a href="/" className="test-decoration-none text-white">Books</a>
+        </h4>
+      </div>
+      {/**TODO: Menu */}
+    </header>
+  )
+}
+
+export default NavBAr
