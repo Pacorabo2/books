d86607f9e76704880ae19745cc3155d68a0cba21
Nicolas Plaza <pacorabo2@hotmail.com>
2021-01-13 21:26:54 +0100

    actions creation on new file: actionFetchBooks.js

---
+++ src/redux/actions/actionFetchBooks.js
@@ -0,0 +1,22 @@
+import {FETCH_BOOKS_LOADING, FETCH_BOOKS_SUCCESS, FETCH_BOOKS_ERROR} from '../constants';
+
+
+const fetchBooksLoading = () => {
+  return {
+    type: FETCH_BOOKS_LOADING
+  }
+}
+
+const fetchBooksSuccess = data => {
+  return {
+    type: FETCH_BOOKS_SUCCESS, 
+    payload: data
+  }
+}
+
+const fetchBooksError = error => {
+  return {
+    type: FETCH_BOOKS_ERROR,
+    payload: error
+  }
+}
