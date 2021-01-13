f6ce6bb371df03e5095bddfe9ab5db91706da4e7
Nicolas Plaza <pacorabo2@hotmail.com>
2021-01-13 19:20:15 +0100

    Implementation search form of SearchBook container

--- src/containers/SearchBooks.js
+++ src/containers/SearchBooks.js
@@ -2,9 +2,36 @@ import React from 'react';
 
 const SearchBooks = () => {
   return (
-    <div>
-      Fetch books from google Api
-    </div>
+    <main role="main">
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container text-center">
+          <h1 className="display-4">BOOKS</h1>
+          <p>Ajouter un livre à votre bibliothèque</p>
+
+          <form className="form-inline justify-content-center">
+            <div className="form-group">
+              <input 
+                type="text" 
+                className="form-control"
+                placeholder="Titre du Livre"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input 
+                type="text" 
+                className="form-control ml-3"
+                placeholder="Auteur"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <button className="btn btn-outline-secondary ml-3">Ajouter un livre</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </main>
   )
 }
 
